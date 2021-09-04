9f7bfa1f84d953be939885269456c42bd4769537
Sholde <exshode@gmail.com>
2021-09-04 16:48:25 +0200

    adding a script to download all scripts and setting them executable

---
+++ guignolarch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Personal Arch Linux Installation (guignolarch)
+# --------------------------------
+# author    : Sholde
+#             https://github.com/Sholde
+#
+# project   : https://github.com/Sholde/guignolarch
+#
+# license   : GPL-3.0 (http://opensource.org/licenses/GPL-3.0)
+
+set -e
+
+URL=https://raw.githubusercontent.com/Sholde/guignolarch/master
+curl ${URL}/install.sh > install.sh
+curl ${URL}/postchroot.sh > postchroot.sh
+
+echo "Run install.sh script to install Arch Linux"
+
+exit
